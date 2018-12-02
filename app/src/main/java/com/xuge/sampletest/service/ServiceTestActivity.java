aa1a7ef7a0331a76a73e7f4cb823dd9e27e01e57
1993xuge <934755308@qq.com>
2018-12-02 16:38:55 +0800

    init project

---
+++ app/src/main/java/com/xuge/sampletest/service/ServiceTestActivity.java
@@ -0,0 +1,28 @@
+package com.xuge.sampletest.service;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import com.xuge.sampletest.R;
+
+public class ServiceTestActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test_service);
+    }
+
+    public void startService(View view) {
+    }
+
+    public void stopService(View view) {
+    }
+
+    public void bindService(View view) {
+    }
+
+    public void unbindService(View view) {
+    }
+}
